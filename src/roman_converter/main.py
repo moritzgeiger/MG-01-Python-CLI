0429c74fc46bfc750ea6a178e3718615761ed7d4
Moritz Geiger <moritzgeiger@hotmail.de>
2023-11-02 13:31:37 +0100

    init

---
+++ src/roman_converter/main.py
@@ -0,0 +1,31 @@
+import sys
+import typer
+
+from roman_converter import utils
+
+def main():
+    """Identifies if input is Roman or Arabic and executes conversion functions for either of them.
+    """
+
+    # instanciating user input
+    args = sys.argv
+
+    if len(args) < 2:
+        print(f"Please provide valid integer or string.")
+        sys.exit()
+
+    m = args[1]
+    print('Running Roman numeral converter...')
+
+    if m.isnumeric():
+        print('Input was Arabic.')
+        _ = utils.arabic_to_roman(int(m))
+        sys.exit()
+
+    if isinstance(m, str):
+        print('Input was Roman.')
+        _ = utils.roman_to_arabic(m)
+        sys.exit()
+
+if __name__ == "__main__":
+    typer.run(main)
---
+++ src/roman_converter/utils.py
@@ -0,0 +1,81 @@
+import sys
+
+ROMAN_MAPPING = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+}
+
+ARABIC_MAPPING = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: 'X',
+        9: 'IX',
+        5: 'V',
+        4: 'IV',
+        1: 'I'
+    }
+
+def roman_to_arabic(roman: str):
+    """Converts any Roman number to a numeric value
+
+    Args:
+        roman (str): The input Roman number
+    """
+    prev_result = 0
+    result = 0
+    reps = 0
+
+    # evaluating Roman number
+    if all([i in ROMAN_MAPPING for i in roman]):
+        for r in roman[::-1]: 
+            current_result = ROMAN_MAPPING.get(r)
+            reps += 1 if current_result == prev_result else reps
+            if reps >= 3:
+                print(f"Invalid Roman number. Your input: {roman}")
+                sys.exit()
+
+            if current_result >= prev_result:
+                result += current_result
+            
+            else:
+                result -= current_result
+
+            prev_result = current_result
+        
+        print(result)
+    
+    else:
+        print(f"Invalid Roman number: {roman}")
+        sys.exit()
+
+
+def arabic_to_roman(arabic: int):
+    """Converts any Roman number to a numeric value
+
+    Args:
+        arabic (str): The input Arabic number
+    """
+    if not all([arabic > 0,  arabic < 3999]):
+        print(f"Input should be between zero and 3999. Your input: {arabic}")
+        sys.exit()
+
+    result = ''
+    for value, numeral in ARABIC_MAPPING.items():
+        while arabic >= value:
+            result += numeral
+            arabic -= value
+
+
+    print(result)
+
